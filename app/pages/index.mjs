d4f89b62957151c51b1bf30fd701fb6296e90310
macdonst <simon.macdonald@gmail.com>
2023-04-25 22:22:08 -0400

    Fixed display name when logged in
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/pages/index.mjs
+++ app/pages/index.mjs
@@ -1,10 +1,11 @@
 export default function({ html, state }) {
   const { authorized } = state.store
+  console.log(state.store)
   return html`
 <enhance-page-container>
   <nav-menu></nav-menu>
   <h1>HOME</h1>
-  ${authorized ? `<p>Logged in as ${authorized.firstname} ${authorized.lastname}</p>` : `<p>Not Logged In</p>`}
+  ${authorized ? `<p>Logged in as ${authorized.displayName}</p>` : `<p>Not Logged In</p>`}
   <p>Details:</p>
   <p>${JSON.stringify(authorized)}</p>
 </enhance-page-container>
