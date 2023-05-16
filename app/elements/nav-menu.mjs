543b11a931df336a16a79375348832d5070569f4
kj <kj@begin.com>
2023-05-16 15:48:02 -0700

    Updates nav menu element

--- app/elements/nav-menu.mjs
+++ app/elements/nav-menu.mjs
@@ -4,19 +4,33 @@ export default function NavMenu({ state, html }) {
   return html`
 <nav class="flex flex-row justify-content-between align-items-center">
   <ul class="m1 list-none flex flex-row align-items-center">
-    <li class="mi-1"><enhance-link href='/'><strong>example.com</strong></enhance-link></li>
+    <li class="mi-1">
+      <enhance-link href='/'><strong>example.com</strong></enhance-link>
+    </li>
   </ul>
   <ul class="m1 list-none flex flex-row align-items-center">
     ${permissions?.includes('admin') ? `
-    <li class="mi-1"><enhance-link href="/accounts">Accounts</enhance-link></li>
+    <li class="mi-1">
+      <enhance-link href="/accounts">Accounts</enhance-link>
+    </li>
     ` : '' }
     ${!permissions ? `
-    <li class="mi-1"><enhance-link href="/register">Register</enhance-link></li>
-    <li class="mi-1"><enhance-link href="/login">Login</enhance-link></li>
+    <li class="mi-1">
+      <enhance-link href="/register">Sign-up</enhance-link>
+    </li>
+    <li class="mi-1">
+      <enhance-link-button href="/login">
+        Login
+      </enhance-link-button>
+    </li>
     ` : '' }
     ${permissions ? `
     <form action=/logout method=post>
-      <li class="mi-1"><enhance-submit-button><span slot=label>Logout<span></enhance-submit-button></li>
+      <li class="mi-1">
+        <enhance-submit-button>
+          <span slot="label">Logout</span>
+        </enhance-submit-button>
+      </li>
     </form>
     ` : '' }
   </ul>
