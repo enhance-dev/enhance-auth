d904e8d8bf4e8dbce026031a0493463b804e2a0d
kj <kj@begin.com>
2023-05-16 14:28:56 -0700

    Updates nav-menu to new Enhance styles

--- app/elements/nav-menu.mjs
+++ app/elements/nav-menu.mjs
@@ -1,22 +1,22 @@
 export default function NavMenu({ state, html }) {
-  const { authorized = {} } = state.store 
+  const { authorized = {} } = state.store
   const permissions = authorized.scopes
   return html`
-<nav class="flex flex-row justify-between items-center">
-  <ul class="m1 list-none flex flex-row items-center">
-    <li class="ml-1 mr-1"><enhance-link href='/'><strong>example.com</strong></enhance-link></li>
+<nav class="flex flex-row justify-content-between align-items-center">
+  <ul class="m1 list-none flex flex-row align-items-center">
+    <li class="mi-1"><enhance-link href='/'><strong>example.com</strong></enhance-link></li>
   </ul>
-  <ul class="m1 list-none flex flex-row items-center">
+  <ul class="m1 list-none flex flex-row align-items-center">
     ${permissions?.includes('admin') ? `
-    <li class="ml-1 mr-1"><enhance-link href="/accounts">Accounts</enhance-link></li>
+    <li class="mi-1"><enhance-link href="/accounts">Accounts</enhance-link></li>
     ` : '' }
     ${!permissions ? `
-    <li class="ml-1 mr-1"><enhance-link href="/register">Register</enhance-link></li>
-    <li class="ml-1 mr-1"><enhance-link href="/login">Login</enhance-link></li>
+    <li class="mi-1"><enhance-link href="/register">Register</enhance-link></li>
+    <li class="mi-1"><enhance-link href="/login">Login</enhance-link></li>
     ` : '' }
     ${permissions ? `
     <form action=/logout method=post>
-      <li class="ml-1 mr-1"><enhance-submit-button><span slot=label>Logout<span></enhance-submit-button></li>
+      <li class="mi-1"><enhance-submit-button><span slot=label>Logout<span></enhance-submit-button></li>
     </form>
     ` : '' }
   </ul>
