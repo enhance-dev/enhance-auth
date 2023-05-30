b5d1f5395c8d27c2e9a28864789771628b4aedbe
Cole Peters <cole@colepeters.com>
2023-05-30 15:53:03 -0500

    Update magic link UI

--- app/pages/login/magic-link.mjs
+++ app/pages/login/magic-link.mjs
@@ -3,18 +3,22 @@ export default function login({ html, state }) {
   const problems = state.store.problems || {}
   const login = state.store.login || {}
   return html`
-  <enhance-page-container>
-    <nav-menu></nav-menu>
+  <page-container>
     <main>
-      <h1 class="mb1 font-semibold text2">Log in</h1>
-      <div class="${problems.form ? 'block' : 'hidden'}">
-        <p>Found some problems!</p>
-        <ul>${problems.form}</ul>
-      </div>
-      <enhance-form action="/login/magic-link" method="post">
-        <enhance-text-input label="Email" id="email" name="email" type="email" errors="${problems?.email?.errors || ''}" value="${login?.email || ''}"></enhance-text-input>
-        <enhance-submit-button style="float: right"><span slot="label">Login</span></enhance-submit-button>
-    </enhance-form>
+      <form-container>
+        <enhance-fieldset legend="Log in">
+          <div class="${problems.form ? 'block' : 'hidden'}">
+            <p>Found some problems!</p>
+            <ul>${problems.form}</ul>
+          </div>
+          <enhance-form action="/login/magic-link" method="post">
+            <enhance-text-input label="Email" id="email" name="email" type="email" errors="${problems?.email?.errors || ''}" value="${login?.email || ''}"></enhance-text-input>
+            <div class="text-end">
+              <enhance-submit-button><span slot="label">Send magic link</span></enhance-submit-button>
+            </div>
+          </enhance-form>
+        </enhance-fieldset>
+      </form-container>
     </main>
-  </enhance-page-container>
+  </page-container>
   ` }
