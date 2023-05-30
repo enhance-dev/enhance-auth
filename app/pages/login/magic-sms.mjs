f04f07c2c01a959ac149cffb608d44be8fbc9e5b
Cole Peters <cole@colepeters.com>
2023-05-30 16:19:56 -0500

    Fix removal of retry form 😅

--- app/pages/login/magic-sms.mjs
+++ app/pages/login/magic-sms.mjs
@@ -4,6 +4,19 @@ export default function login({ html, state }) {
   const problems = state.store.problems || {}
   const login = state.store.login || {}
   return html`
+    <style>
+      .primary500 {
+        color: var(--primary-500);
+      }
+
+      .float-left {
+        float: left;
+      }
+
+      .float-right {
+        float: right;
+      }
+    </style>
     <page-container>
       <main>
         <form-container>
@@ -23,10 +36,10 @@ export default function login({ html, state }) {
             ${smsCodeLogin ? `
             <enhance-form action="/login/magic-sms" method="post">
               <enhance-text-input label="Magic code" id="smsCode" name="smsCode" type="password"></enhance-text-input>
-              <div class="flex align-items-center justify-content-between">
-                <enhance-link href="/login/magic-sms?retry">Send new code</enhance-link>
-                <enhance-submit-button><span slot="label">Log in</span></enhance-submit-button>
-              </div>
+              <enhance-submit-button class="float-right"><span slot="label">Log in</span></enhance-submit-button>
+            </enhance-form>
+            <enhance-form action="/login/magic-sms?retry" method="post" class="float-left">
+              <button type="submit" class="pb-3 primary500 underline">Send new code</button>
             </enhance-form>
             ` : ''}
           </enhance-fieldset>
